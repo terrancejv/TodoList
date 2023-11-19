5390e1babb4bff1ee88e86da7803ca8c9c156d3e
Terrance Valdez <terrancejvaldez@gmail.com>
2023-11-19 01:00:33 -0800

    update

--- api/server.js
+++ api/server.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mern-todo', {
+/* old uri mongodb://127.0.0.1:27017/mern-todo */
+const mongoDBURI = process.env.MONGODB_URI;
+mongoose.connect(mongoDBURI, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
